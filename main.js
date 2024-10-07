cfed9debf224daf7a8527d2ee85f2cb9af4fb18e
Herbert Dunbar Jr <herbertdunbar19@gmail.com>
2024-10-07 13:44:06 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+a=6
+b=7               // variable
+c=a+b            // expression a+b adding a and b, then assigning to variable c
+console.log(c)  // passing c into the log method/function of the console object.
+
+//F5+5 RUN/PLAY    
+
+let firstname = "Kevin"; // JavaScripts's fundamental/primative data types,
+let greeting = "Hello "; // include numbers and strings of characters/letter.
+
+// Concatenate operator - "Chain together" like cars a train.
+let message = greeting + firstname; // "Hello Kevin"
+
+console.log(message); // "Hello Kevin"
