97b19ffc58f51b426786eef9be7cae7470f6c5b7
Herbert Dunbar Jr <herbertdunbar19@gmail.com>
2024-10-09 12:06:24 -0400

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+
+// COMPUTER VALUES ARE 0 and 1, AKA OFF and ON
+// LOGIC TRUE OR FALSE< 0 is usually false and non-zero is usually true
+
+// Bool - Boolean (True/False) Data Type - Reason/Philosophy into math?
+
+// INPUTS
+hasCar = true
+hasLicense = true
+canReachPedals = true
+
+//CALCULATION
+console.log(hasLicense)
+
+//DECISION
+if(hasLicense && hasCar){ //acts like a gate 
+    console.log("Zoom Zoom"); // did not run?!?!
+}
+
