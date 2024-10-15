3922f7e0c359fb213475bf78e75c9710fde7253c
Herbert Dunbar Jr <herbertdunbar19@gmail.com>
2024-10-14 22:05:57 -0400

    update

--- area/area.js
+++ area/area.js
@@ -8,18 +8,18 @@
 //area is the identifier/name
 //width and height are the received parameters
 function area(width, height){
-    // calculate and then send back the result to the caller
+    //calculate and then send back the result to the caller
     const resultArea = width * height; //calculate
     return resultArea; //send back to the caller
 }
 
-//call function by name, pass both arguments being passed
-console.log(area(10, 20)); // 10 and 20 are the arguments being passed
-//two more times!!
+//call function by name, pass both arguments, and recieve and print result
+console.log(area(10, 20));// 10 and 20 are the arguments being passed
+//two more times!
 console.log(area(12, 20));
 console.log(area(24, 40));
 
 // let height = 10
 // let width = 20
-// let  myArea = area(width, height)
+// let myArea = area(width, height)
 // console.log(myArea)
